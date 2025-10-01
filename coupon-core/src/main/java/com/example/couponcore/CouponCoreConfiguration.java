53fbc1a48a3c356ebcc5e0d4dea8c4a605ba75e3
kjp0411 <98109773+kjp0411@users.noreply.github.com>
2025-10-01 15:02:09 +0900

    프로젝트 구성

---
+++ coupon-core/src/main/java/com/example/couponcore/CouponCoreConfiguration.java
@@ -0,0 +1,10 @@
+package com.example.couponcore;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@EnableAutoConfiguration
+public class CouponCoreConfiguration {
+
+}
