db19d7794f6e6fd2c42967774d7a28f83edf7015
kjp0411 <98109773+kjp0411@users.noreply.github.com>
2025-10-09 22:59:05 +0900

    Coupon 엔티티 설계 및 작성

---
+++ coupon-core/src/main/java/com/example/couponcore/model/Coupon.java
@@ -0,0 +1,52 @@
+package com.example.couponcore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+@Table(name = "coupons")
+public class Coupon extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private CouponType couponType;
+
+    private Integer totalQuantity;
+
+    @Column(nullable = false)
+    private int issuedQuantity;
+
+    @Column(nullable = false)
+    private int discountAmount;
+
+    @Column(nullable = false)
+    private int minAvailableAmount;
+
+    @Column(nullable = false)
+    private LocalDateTime dateIssueStart;
+
+    @Column(nullable = false)
+    private LocalDateTime dateIssueEnd;
+}
